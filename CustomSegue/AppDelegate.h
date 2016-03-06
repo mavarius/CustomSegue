1a664f2319014a63d042281405656896875c1bea
Daniel Campbell <metalwhirlwind@hotmail.com>
2016-03-05 20:15:13 -0800

    Initial Commit

---
+++ CustomSegue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomSegue
+//
+//  Created by Daniel Campbell on 3/5/16.
+//  Copyright © 2016 Daniel Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomSegue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomSegue
+//
+//  Created by Daniel Campbell on 3/5/16.
+//  Copyright © 2016 Daniel Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
