1a412d902e5d23566d936a8f24a07087189d3e9d
Daniel Campbell <Mavarius@Davids-MacBook-Pro-2.local>
2016-05-01 18:48:52 -0700

    Half Animation
    The animation halfway works, a good place to commit.

---
+++ DerivedData/CustomSegue/Build/Intermediates/CustomSegue.build/Debug-iphoneos/CustomSegue.build/Objects-normal/arm64/ViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Mavarius/Desktop/Xcode\ Playground/CustomSegue/CustomSegue/ViewController.m \
+  /Users/Mavarius/Desktop/Xcode\ Playground/CustomSegue/CustomSegue/ViewController.h
---
+++ DerivedData/CustomSegue/Build/Intermediates/CustomSegue.build/Debug-iphoneos/CustomSegue.build/Objects-normal/arm64/main.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/Mavarius/Desktop/Xcode\ Playground/CustomSegue/CustomSegue/main.m \
+  /Users/Mavarius/Desktop/Xcode\ Playground/CustomSegue/CustomSegue/AppDelegate.h \
+  /Users/Mavarius/Desktop/Xcode\ Playground/CustomSegue/CustomSegue/ViewController.h
